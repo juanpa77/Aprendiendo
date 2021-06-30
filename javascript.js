27048128c14fbd781db3c5dc7d7db1264fdbae69
Juan <juanpa.box.77@gmail.com>
2021-06-29 23:02:51 -0300

    Prueba

---
+++ javascript.js
@@ -0,0 +1,31 @@
+
+const canvas = document.getElementById("lienzo");
+let button = document.getElementById("mover");
+let x = 40;
+let y = 0;
+let ancho = 20;
+let largo = 30;
+let ctx = canvas.getContext("2d");
+
+
+function inter() {
+    let intervalo = setInterval(mover,100);
+}
+function draw() {
+    ctx.beginPath();
+    ctx.fillRect(x, y, ancho, largo);
+    ctx.closePath();
+    ctx.clearRect(x,y-ancho,ancho,largo);
+}
+function mover(){ 
+    draw();
+       
+    y == 250 ? (clearInterval(inter), setInterval(draw, 100), y -= 5)
+    : y += 5;
+    
+}
+
+
+button.addEventListener("click",inter);
+
+
